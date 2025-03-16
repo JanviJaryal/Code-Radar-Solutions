fcc06824b87e38ccf81e67a1a7ec42585c055476
JanviJaryal <janvi1104.becse24@chitkara.edu.in>
2025-03-16 23:56:01 +0530

    Solution to Fibonacci Series

---
+++ Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void fibonacciSeries(int n){
+     if(n==1){
+         printf("0");
+     }
+     }else{
+     int a=0;
+     int b=1;
+     printf("%d %d",a,b);
+     for(int i=2;i<n;i++){
+         int c=a+b;
+         a=b;
+         b=c;
+         printf(" %d",c);
+     }
+     }
+ 
